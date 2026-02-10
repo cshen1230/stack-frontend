022b0b3c8d4dc841eedbdcb7949428b609a79265
Chris Shen <cshen1230@gmail.com>
2026-02-09 17:53:05 -0800

    Initial Commit

---
+++ StackPickleball/StackPickleballApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackPickleballApp.swift
+//  StackPickleball
+//
+//  Created by Chris Shen on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct StackPickleballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
